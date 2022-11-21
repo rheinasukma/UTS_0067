830865ce42accc07c1d0dd8f2a050a29849c03fe
rheinasukma <rheinaanggini05@gmail.com>
2022-11-21 17:43:27 +0700

    ubah judul

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'UTS 0067 Flutter Demo',
+      title: 'UTS 0067 Rheina',
       theme: ThemeData(
         // This is the theme of your application.
         //
